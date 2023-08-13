57f0a638a8acadca6e7a6ce2eb161bc3ebaae933
josemlegal <josemlegal97@gmail.com>
2023-08-13 17:18:56 -0400

    Add router

---
+++ lib/core/router/app_router.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:my_budget/core/barell/views.dart';
+
+class Router {
+  //Auth views
+  static const landingView = '/landing';
+  // static const onboardingView = '/onboarding';
+  // static const forgotPasswordView = '/forgot-password';
+
+  // static const homeView = '/home';
+  // static const profileView = '/profile';
+  // static const tabsView = '/tabs';
+  // static const subscriptionView = '/subscription-view';
+
+  static Route<dynamic>? generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case landingView:
+        return MaterialPageRoute(builder: (_) => const LandingView());
+      // case homeView:
+      //   return MaterialPageRoute(
+      //     builder: (_) => const HomeView(),
+      //   );
+      // case profileView:
+      //   return MaterialPageRoute(builder: (_) => const ProfileView());
+
+      // case onboardingView:
+      //   return MaterialPageRoute(builder: (_) => const FormView());
+
+      // case tabsView:
+      //   return MaterialPageRoute(builder: (_) => const TabsView());
+
+      // case forgotPasswordView:
+      //   return MaterialPageRoute(builder: (_) => const ForgotPasswordView());
+      // case subscriptionView:
+      //   return MaterialPageRoute(builder: (_) => const SubscriptionView());
+      default:
+        return null;
+    }
+  }
+}
