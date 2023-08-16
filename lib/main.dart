a3e9aef6a0be20461a2400f8fdb5ddfbcbdcac3d
josemlegal <josemlegal97@gmail.com>
2023-08-15 20:53:09 -0400

    Add packages and some changes in the main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,38 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:my_budget/application/services/shared_preferences_service.dart';
+import 'package:my_budget/core/dependency_injection/locator.dart';
+import 'package:my_budget/core/theme/app_theme.dart';
 import 'package:my_budget/firebase_options.dart';
 import 'package:my_budget/core/router/app_router.dart' as router;
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MainApp());
+  final sharedPreferences = locator<SharedPreferenceApi>();
+  await sharedPreferences.init();
+  setupLocator();
+  runApp(
+    const ProviderScope(child: MainApp()),
+  );
 }
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
+  bool get isUserLoggedIn => FirebaseAuth.instance.currentUser != null;
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      initialRoute: router.Router.landingView,
+    return MaterialApp(
+      theme: AppTheme().getTheme(),
+      debugShowCheckedModeBanner: false,
+      initialRoute:
+          isUserLoggedIn ? router.Router.tabsView : router.Router.landingView,
       onGenerateRoute: router.Router.generateRoute,
     );
   }
